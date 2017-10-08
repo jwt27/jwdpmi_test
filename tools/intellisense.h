dfd95ebdb7d0b0d0dfe34ea7b64af144ab38b66d
J.W. Jagersma <jwjagersma@gmail.com>
2017-10-08 14:30:59 +0200

    intellisense works reasonably well now in VS2017.

--- tools/intellisense.h
+++ tools/intellisense.h
@@ -2,6 +2,24 @@
 
 #ifdef __INTELLISENSE__
 
+#undef __cplusplus
+#undef __cpp_attributes
+#undef __cpp_aggregate_nsdmi
+#undef __cpp_variable_templates
+#undef __cpp_delegating_constructors
+#undef __cpp_raw_strings
+#undef __cpp_ref_qualifiers
+#undef __cpp_initializer_lists
+#undef __cpp_lambdas
+#undef __cpp_decltype_auto
+#undef __cpp_sized_deallocation
+#undef __cpp_unicode_literals
+#undef __cpp_return_type_deduction
+#undef __cpp_decltype
+#undef __cpp_alias_templates
+#undef __cpp_generic_lambdas
+#undef __cpp_user_defined_literals
+#undef __cpp_init_captures
 #undef __cpp_exceptions
 #undef __cpp_enumerator_attributes
 #undef __cpp_binary_literals
@@ -14,5 +32,12 @@
 #undef __CHAR32_TYPE__
 
 #include "gcc_defines.h"
+
+#undef __SIZE_TYPE__
+#define __SIZE_TYPE__ size_t
+using size_t = size_t; // yes...
+
+#define __float128 long double
+#define __null 0
 
 #endif
