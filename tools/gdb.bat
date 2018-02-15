85f14b85ad0fed7303e1ebb05324105d385ddfda
J.W. Jagersma <jwjagersma@gmail.com>
2018-02-15 16:15:19 +0100

    debugging in visual studio appears to work now.

--- tools/gdb.bat
+++ tools/gdb.bat
@@ -1,2 +1,2 @@
 SET MSYSTEM=MINGW64
-%MSYS2_ROOT%\usr\bin\bash --login -c "/%MSYS2_DJGPP%/bin/i586-pc-msdosdjgpp-gdb.exe %*"
+%MSYS2_ROOT%\usr\bin\bash --login -c "%MSYS2_DJGPP%/bin/i586-pc-msdosdjgpp-gdb %*"
