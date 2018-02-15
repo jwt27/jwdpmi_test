2019027537a5fd8ec3d6c8cb6c4e376ebf700486
J.W. Jagersma <jwjagersma@gmail.com>
2018-02-15 18:14:33 +0100

    call make from a separate batch file, to work around a long standing issue in VS2017 where the working directory defaults to %USERPROFILE%/source

--- make_vs.sh
+++ make_vs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export PATH=/$MSYS2_DJGPP/i586-pc-msdosdjgpp/bin:$PATH;
-export GCC_EXEC_PREFIX=/$MSYS2_DJGPP/lib/gcc/;
+export PATH=/$MSYS2_DJGPP/bin:$PATH;
 unset CXXFLAGS
 unset CFLAGS
 make -Otarget -j`nproc --all` vs $@
